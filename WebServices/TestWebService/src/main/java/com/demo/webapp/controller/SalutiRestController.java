c1bbf9f32f978abd9576757c9c3bbc3930185846
Yusdel <48959388+Yusdel@users.noreply.github.com>
2021-03-21 19:08:55 +0100

    First Rest and Test JUnit

---
+++ WebServices/TestWebService/src/main/java/com/demo/webapp/controller/SalutiRestController.java
@@ -0,0 +1,14 @@
+package com.demo.webapp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class SalutiRestController {
+	@GetMapping(value = "/test")
+	public String getGreetings() {
+		return ("Saluti, sono Yusdel!");
+	}
+}
