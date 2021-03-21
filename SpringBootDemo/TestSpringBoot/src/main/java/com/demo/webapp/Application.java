5e5938998af5ed451d4687a090613574559fcb84
Yusdel <48959388+Yusdel@users.noreply.github.com>
2021-03-21 17:24:39 +0100

    Starter Project
    Web MVC

---
+++ SpringBootDemo/TestSpringBoot/src/main/java/com/demo/webapp/Application.java
@@ -0,0 +1,14 @@
+package com.demo.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	/*Entry Point*/
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
