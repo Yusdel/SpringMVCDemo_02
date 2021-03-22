2063b6b3ab50874ba10c72080e0fc9a519f7e923
Yusdel <48959388+Yusdel@users.noreply.github.com>
2021-03-22 09:56:55 +0100

    description

--- WebServices/TestWebService/src/main/java/com/demo/webapp/service/PremiService.java
+++ WebServices/TestWebService/src/main/java/com/demo/webapp/service/PremiService.java
@@ -2,6 +2,9 @@ package com.demo.webapp.service;
 
 /*
  * TODO Code Injection
+ * 
+ * A fronte di un'unica interfaccia è possibile avere più classi di implementazione che
+ * poi potranno essere usate direttamente nel codice della nostra business login 
  */
 public interface PremiService {
 	public double GetPremio();
