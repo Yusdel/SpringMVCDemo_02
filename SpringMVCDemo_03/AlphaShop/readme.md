a48b781d5a497e20cb9ae4482dcdfd86543c3f8c
Yusdel <48959388+Yusdel@users.noreply.github.com>
2021-03-23 17:01:41 +0100

    TODO Spring Security

--- SpringMVCDemo_03/AlphaShop/readme.md
+++ SpringMVCDemo_03/AlphaShop/readme.md
@@ -5,3 +5,5 @@ This file represents a hypothetical terminal file to be read.
 > **42995** ( _Terminal number_ )
 
 > **002** ( _Number of the store_ )
+
+### https://www.baeldung.com/spring-data-derived-queries
---
+++ SpringMVCDemo_03/AlphaShop/src/main/java/com/demo/webapp/config/WebAppSecurityInitializer.java
@@ -0,0 +1,11 @@
+package com.demo.webapp.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+/*
+ * TODO Spring Security | new Class
+ */
+public class WebAppSecurityInitializer extends AbstractSecurityWebApplicationInitializer
+{
+
+}
